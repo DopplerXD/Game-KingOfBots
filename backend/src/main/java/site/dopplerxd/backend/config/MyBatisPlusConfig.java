70e7abd64c6051a1b76efbdbd1bbfe2655685284
DopplerXD <1509209607@qq.com>
2024-12-05 16:24:34 +0800

    前后端初始化

---
+++ backend/src/main/java/site/dopplerxd/backend/config/MyBatisPlusConfig.java
@@ -0,0 +1,17 @@
+package site.dopplerxd.backend.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor paginationIntercepter() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        interceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return interceptor;
+    }
+}
---
+++ backend/src/main/java/site/dopplerxd/backend/config/WebConfig.java
@@ -0,0 +1,18 @@
+package site.dopplerxd.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 允许访问的路径
+                .allowedOrigins("http://localhost:8080") // 允许的来源
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 允许的方法
+                .allowedHeaders("*") // 允许的请求头
+                .allowCredentials(true); // 是否允许发送 Cookie
+    }
+}
